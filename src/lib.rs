5fdea2204207d71a60d0eb5199e8184559ddc47d
Fredrik Fornwall <fredrik@fornwall.net>
2020-01-22 22:38:53 +0100

    Treat android as linux
    This works for Termux since openssl-probe got Termux support in
    https://github.com/alexcrichton/openssl-probe/commit/d990f3380cfefee0277c4f173f60d4e32bced615

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@
 //! It consists of a single function [load_native_certs](fn.load_native_certs.html) which returns a
 //! `rustls::RootCertStore` pre-filled from the native certificate store.
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "android", target_os = "android"))]
 mod linux;
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "android", target_os = "android"))]
 use linux as platform;
 
 #[cfg(target_family = "windows")]
